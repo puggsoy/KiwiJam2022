d839b6766d14014a896fd4b96809bc8e2bed114a
Sami Habib <puggsoy@gmail.com>
2022-07-23 18:59:11 +1200

    Expanded card definition

--- Assets/Scripts/CardDefinitions.cs
+++ Assets/Scripts/CardDefinitions.cs
@@ -20,6 +20,14 @@ public class CardDefinitions : ScriptableObject
 [Serializable]
 public class CardDefinition
 {
+	public enum CardType
+	{
+		Triangle,
+		Circle,
+		Square,
+		None
+	}
+
 	[SerializeField]
 	private int m_id = 0;
 	public int ID => m_id;
@@ -27,4 +35,35 @@ public class CardDefinition
 	[SerializeField]
 	private string m_name = null;
 	public string Name => m_name;
+
+	[SerializeField]
+	private string m_description = null;
+	public string Description => m_description;
+
+	[SerializeField]
+	private CardEffect m_effect = null;
+	public CardEffect Effect => m_effect;
+
+	[SerializeField]
+	private CardType m_type = CardType.Triangle;
+	public CardType Type => m_type;
+
+	[SerializeField]
+	private Sprite m_image = null;
+	public Sprite Image => m_image;
+}
+
+[Serializable]
+public class CardEffect
+{
+	public enum OperationType
+	{
+		Add,
+		Subtract,
+		Multiply
+	}
+
+	public OperationType Operation = OperationType.Add;
+
+	public int Amount = 1;
 }
