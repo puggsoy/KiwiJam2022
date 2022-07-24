a996f21cfc6ec0f182a359ba757b93b20486af4e
Sami Habib <puggsoy@gmail.com>
2022-07-24 16:36:41 +1200

    Added and hooked up card images

--- Assets/Scripts/CardGameObject.cs
+++ Assets/Scripts/CardGameObject.cs
@@ -4,12 +4,24 @@ using UnityEngine.UI;
 
 public class CardGameObject : MonoBehaviour
 {
-
 	[SerializeField]
 	private Image m_image = null;
 
 	[SerializeField]
 	private Text m_text = null;
+
+	[Space]
+	[SerializeField]
+	private Sprite m_triangleSprite = null;
+
+	[SerializeField]
+	private Sprite m_circleSprite = null;
+
+	[SerializeField]
+	private Sprite m_squareSprite = null;
+
+	[SerializeField]
+	private Sprite m_colourlessSprite = null;
 
 	private int m_cardId = -1;
 
@@ -23,7 +35,22 @@ public class CardGameObject : MonoBehaviour
 		{
 			m_cardId = card.ID;
 			m_text.text = card.Definition.Name;
-			m_image.color = CardDefinition.TypeToColor[card.Definition.Type];
+			
+			switch (card.Definition.Type)
+			{
+				case CardDefinition.CardType.Triangle:
+					m_image.sprite = m_triangleSprite;
+					break;
+				case CardDefinition.CardType.Circle:
+					m_image.sprite = m_circleSprite;
+					break;
+				case CardDefinition.CardType.Square:
+					m_image.sprite = m_squareSprite;
+					break;
+				case CardDefinition.CardType.None:
+					m_image.sprite = m_colourlessSprite;
+					break;
+			}
 		}
 
 		gameObject.SetActive(m_cardId >= 0);
