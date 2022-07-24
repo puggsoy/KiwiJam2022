be9154ba9cbee89c510919d9568385bc1df10da2
Tim Donnan <t.donnan@live.com.au>
2022-07-24 13:41:35 +1200

    Added instructions to menu scene

--- Assets/Scripts/MenuScreen.cs
+++ Assets/Scripts/MenuScreen.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MenuScreen : MonoBehaviour
 {
+	[SerializeField]
+	private GameObject m_intructionsPrefab = null;
+
     public void OnStartClick()
     {
         Invoke("StartGame", 0.4f);
@@ -27,5 +30,15 @@ public class MenuScreen : MonoBehaviour
     #if UNITY_EDITOR
 		Debug.LogWarning("Would quit in actual game executable");
     #endif
+	}
+
+	public void OpenInstructions()
+	{
+		m_intructionsPrefab.SetActive(true);
+	}
+
+	public void CloseInstructions()
+	{
+		m_intructionsPrefab.SetActive(false);
 	}
 }
